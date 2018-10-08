4a8b575eac3b85e80bf95b90759e4907851ce3cd
StevenMHernandez <contact@stevenhdesigns.com>
2018-10-08 12:44:23 -0400

    assignment1: update run script, let's just hope it doesn't break too much

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -20,12 +20,12 @@ for ((i=10;i<=250;i++)); do
     ./threaded tmp.txt >> tmp/thread_runs.txt
 done
 
-## Time tests (mpi)
-#echo "\c" > tmp/mpi_runs.txt
-#for ((i=10;i<=100;i++)); do
-#    sh gen_cities.sh $i > tmp.txt
-#    echo "run $i: \c" >> tmp/mpi_runs.txt
-#    ./mpi tmp.txt >> tmp/mpi_runs.txt
-#done
+# Time tests (mpi)
+echo "\c" > tmp/mpi_runs.txt
+for ((i=10;i<=250;i++)); do
+    sh gen_cities.sh $i > tmp.txt
+    echo "run $i: \c" >> tmp/mpi_runs.txt
+    mpirun -np 4 ./mpi tmp.txt >> tmp/mpi_runs.txt
+done
 
 echo "done"

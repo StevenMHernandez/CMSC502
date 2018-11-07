d54382348d979cfb596cb449247bc2b4c2e398e4
StevenMHernandez <contact@stevenhdesigns.com>
2018-11-07 15:28:33 -0500

    assignment2: make number of points per process configurable, add timing test shell script for judging the time it takes to run for different configurations

---
+++ assignment2/timing_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#
+# Run this file as an executable as `./timing_test.sh &` so that it runs in the background!
+#
+
+make clean
+make mpi
+
+echo "\c" > tmp/timing_runs.txt
+for ((p=1;p<=6;p++)); do
+    for ((i=10;i<=25;i++)); do
+        sqr=`expr $p \\* $p`
+        mpirun -np $sqr ./mpi $i >> tmp/timing_runs.txt
+    done
+done
